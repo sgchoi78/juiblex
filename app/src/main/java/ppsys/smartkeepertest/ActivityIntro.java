6947d047086cfaf5a24624594aeaaeb747ccca6f
DEV3 <juiblex@naver.com>
2017-03-24 13:03:06 +0900

    dev3 test dd 2

--- app/src/main/java/ppsys/smartkeepertest/ActivityIntro.java
+++ app/src/main/java/ppsys/smartkeepertest/ActivityIntro.java
@@ -1,4 +1,4 @@
-package ppsys.smartkeepersmba;
+package ppsys.smartkeepertest;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -9,5 +9,6 @@ public class ActivityIntro extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        // ghjhjhjh
     }
 }
